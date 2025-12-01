3f8a13a74d3cb6ae4d5bf2a33d71c4c788917e04
Salah Gueroui <salaheddinegueroui50@gmail.com>
2025-12-01 14:32:48 +0100

    Add SQL task solution

---
+++ Course-4-SQL-SELECT-Queries/order-by.sql
@@ -0,0 +1,12 @@
+/*
+Video: Course 4 – timestamp 18:23
+----------------------------------------
+-----SQL-TASK-----
+Retrieve all customers and sort the results by the highest score first.
+*/
+
+------My-solution-----
+
+select *
+from customers
+order by score desc
