fbaeab58526f9737871e9616229cb302e9f76358
Salah Gueroui <salaheddinegueroui50@gmail.com>
2025-12-01 14:57:51 +0100

    Add SQL task solution

--- Course-4-SQL-SELECT-Queries/order-by.sql
+++ Course-4-SQL-SELECT-Queries/order-by.sql
@@ -24,3 +24,17 @@ Retrieve all customers and sort the results by the lowest score first.
 select *
 from customers
 order by score
+
+--------------------------------------------------------------------------------
+/*
+Video: Course 4 – timestamp 22:43
+----------------------------------------
+-----SQL-TASK-----
+Retrieve all customers and sort the results by the country and then by the highest score.
+*/
+
+------My-solution-----
+
+select *
+from customers
+order by country,score desc
