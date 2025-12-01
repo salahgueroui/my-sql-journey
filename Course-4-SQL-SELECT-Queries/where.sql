942e0db02df28629a06404397e245e5f29700c63
Salah Gueroui <salaheddinegueroui50@gmail.com>
2025-12-01 14:07:24 +0100

    Add SQL task solution

---
+++ Course-4-SQL-SELECT-Queries/where.sql
@@ -0,0 +1,12 @@
+/*
+Video: Course 4 – timestamp 12:20
+----------------------------------------
+-----SQL-TASK-01-----
+Retrieve customers with a score not equal to 0.
+*/
+
+------My-solution-----
+
+select *
+from customers
+where score != 0
