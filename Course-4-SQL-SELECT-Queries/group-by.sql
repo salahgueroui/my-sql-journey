843d7130e3f3a4562f951c121c3204566be70265
Salah Gueroui <salaheddinegueroui50@gmail.com>
2025-12-01 16:40:00 +0100

    Add SQL task solution

--- Course-4-SQL-SELECT-Queries/group-by.sql
+++ Course-4-SQL-SELECT-Queries/group-by.sql
@@ -13,4 +13,19 @@ group by country
 order by total_score DESC
 
 --------------------------------------------------------
+/*
+Video: Course 4 – timestamp 32:16
+----------------------------------------
+-----SQL-TASK-----
+Find the total score and total number of customers for each country.
+*/
+
+------My-solution-----
+
+select country,
+	   sum(score)as total_score,
+	   count(distinct(id))as total_customer
+from customers
+group by country
+order by total_score DESC
 
