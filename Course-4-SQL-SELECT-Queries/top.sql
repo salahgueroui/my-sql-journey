9651aa002078bdd46ba371b62057eae5f01d2ba6
Salah Gueroui <salaheddinegueroui50@gmail.com>
2025-12-06 11:51:40 +0100

    Add SQL task solution

---
+++ Course-4-SQL-SELECT-Queries/top.sql
@@ -0,0 +1,10 @@
+/*
+Video: Course 4 – timestamp 46:27
+----------------------------------------
+-----SQL-TASK-----
+Retrieve only 3 customers.
+*/
+
+------My-solution-----
+SELECT top 3 *
+FROM customers
