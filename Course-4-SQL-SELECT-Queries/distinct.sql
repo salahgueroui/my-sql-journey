a2ee6086e54791b402d5a25737c9e486334db881
Salah Gueroui <salaheddinegueroui50@gmail.com>
2025-12-06 11:41:08 +0100

    Add SQL task solution

---
+++ Course-4-SQL-SELECT-Queries/distinct.sql
@@ -0,0 +1,10 @@
+/*
+Video: Course 4 – timestamp 43:09
+----------------------------------------
+-----SQL-TASK-----
+Return unique list of all countries.
+*/
+
+------My-solution-----
+SELECT DISTINCT(country)
+FROM customers
