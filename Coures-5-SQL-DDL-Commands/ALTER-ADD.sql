71d2ea0855e816821f2a3955b55f1eed0a3df5e5
Salah Gueroui <salaheddinegueroui50@gmail.com>
2025-12-07 19:44:15 +0100

    Add SQL solution

---
+++ Coures-5-SQL-DDL-Commands/ALTER-ADD.sql
@@ -0,0 +1,11 @@
+/*
+Video: Course 5 – timestamp 6:58
+----------------------------------------
+-----SQL-TASK-----
+Add a new column called email to the persons table.
+*/
+
+------My-solution-----
+
+alter table persons
+ADD email varchar(50) not null
