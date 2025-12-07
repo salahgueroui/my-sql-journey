9f240ce8fd2cc9bc76241e0cb6539efd1f3823e2
Salah Gueroui <salaheddinegueroui50@gmail.com>
2025-12-07 21:09:29 +0100

    Add SQL solution

---
+++ Coures-5-SQL-DDL-Commands/DROP-TABLE.sql
@@ -0,0 +1,10 @@
+/*
+Video: Course 5 – timestamp 10:23
+----------------------------------------
+-----SQL-TASK-----
+Delete the table persons from the database.
+*/
+
+------My-solution-----
+
+DROP TABLE persons
