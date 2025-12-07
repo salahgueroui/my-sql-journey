d7e5131658841991ae0697fb579add7210d29342
Salah Gueroui <salaheddinegueroui50@gmail.com>
2025-12-07 20:58:20 +0100

    Add SQL solution

---
+++ Coures-5-SQL-DDL-Commands/ALTER-REMOVE.sql
@@ -0,0 +1,11 @@
+/*
+Video: Course 5 – timestamp 8:42
+----------------------------------------
+-----SQL-TASK-----
+Remove the column phone from the persons table.
+*/
+
+------My-solution-----
+
+ALTER TABLE persons
+DROP COLUMN phone;
