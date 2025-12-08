8aadabc7b7de9b4bef2bbf2969aa7aa88fa741c8
Salah Gueroui <salaheddinegueroui50@gmail.com>
2025-12-08 22:51:39 +0100

    Add SQL task solution

---
+++ Course-6-SQL-DML-Commands/DELETE .sql
@@ -0,0 +1,11 @@
+/*
+Video: Course 6 – timestamp 21:07
+----------------------------------------
+-------------- SQL TASK --------------
+Delete all customers with an ID greater than 5
+----------------------------------------
+*/
+
+-------- My-Solution --------
+
+
