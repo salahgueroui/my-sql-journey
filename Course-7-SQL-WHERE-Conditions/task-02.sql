cf0dd09f64786baa2f7dd49180aed970e1224f9a
Salah Gueroui <salaheddinegueroui50@gmail.com>
2025-12-10 18:11:33 +0100

    Add SQL task solution

---
+++ Course-7-SQL-WHERE-Conditions/task-02.sql
@@ -0,0 +1,13 @@
+/*
+Video: Course 7 – timestamp 6:00
+----------------------------------------
+-------------- SQL TASK --------------
+Retrieve all customers who are NOT from Germany
+----------------------------------------
+*/
+
+-------- My-Solution --------
+
+select *
+from customers
+where country != 'germany'
