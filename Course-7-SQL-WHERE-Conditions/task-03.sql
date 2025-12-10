35d4e3cab8038d2d55e9496435c53121db25a470
Salah Gueroui <salaheddinegueroui50@gmail.com>
2025-12-10 18:17:59 +0100

    Add SQL task solution

---
+++ Course-7-SQL-WHERE-Conditions/task-03.sql
@@ -0,0 +1,13 @@
+/*
+Video: Course 7 – timestamp 6:37
+----------------------------------------
+-------------- SQL TASK --------------
+Retrieve all customers with a score greater than 500
+----------------------------------------
+*/
+
+-------- My-Solution --------
+
+select *
+from customers
+where score > 500
