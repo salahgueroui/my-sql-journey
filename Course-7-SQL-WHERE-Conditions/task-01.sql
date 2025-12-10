3266dd1f390290a1cd686f2b9dc6099c0448a43f
Salah Gueroui <salaheddinegueroui50@gmail.com>
2025-12-10 18:07:42 +0100

    Add SQL task solution

---
+++ Course-7-SQL-WHERE-Conditions/task-01.sql
@@ -0,0 +1,13 @@
+/*
+Video: Course 7 – timestamp 5:14
+----------------------------------------
+-------------- SQL TASK --------------
+Retrieve all customers from Germany
+----------------------------------------
+*/
+
+-------- My-Solution --------
+
+select *
+from customers
+where country = 'germany'
