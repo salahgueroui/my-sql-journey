e46500cb362e8307b164c856e6fbb0f04a279f76
Salah Gueroui <salaheddinegueroui50@gmail.com>
2025-12-10 18:32:56 +0100

    Add SQL task solution

--- Course-7-SQL-WHERE-Conditions/Logical-Operators-task-01.sql
+++ Course-7-SQL-WHERE-Conditions/Logical-Operators-task-01.sql
@@ -1,3 +1,12 @@
+/*
+Video: Course 7 – timestamp 12:15
+----------------------------------------
+-----SQL-TASK-----
+Retrieve all customers who are from the USA AND have a score greater than 500.
+*/
+
+------My-solution-----
+
 select *
 from customers
 where score > 500 and country='usa'
