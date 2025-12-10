fa2b31c3228e0fcee20a2daea1002cbbd1dc2e37
Salah Gueroui <salaheddinegueroui50@gmail.com>
2025-12-10 18:40:32 +0100

    Add SQL task solution

---
+++ Course-7-SQL-WHERE-Conditions/Logical-Operators-task-03.sql
@@ -0,0 +1,12 @@
+/*
+Video: Course 7 – timestamp 19:16
+----------------------------------------
+-----SQL-TASK-----
+Retrieve all customers with a score NOT less than 500.
+*/
+
+------My-solution-----
+
+select *
+from customers
+where score >= 500
