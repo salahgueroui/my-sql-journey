0138523ba127292e18e75ae590120b9df5090887
Salah Gueroui <salaheddinegueroui50@gmail.com>
2025-12-11 14:03:18 +0100

    Add SQL task solution

---
+++ Course-7-SQL-WHERE-Conditions/Range-Operator-task1.sql
@@ -0,0 +1,9 @@
+/*
+Video: Course 7 – timestamp 22:51
+----------------------------------------
+-----SQL-TASK-----
+Retrieve all customers whose score falls
+in the range between 100 and 500.
+*/
+
+------My-solution-----
