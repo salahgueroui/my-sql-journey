d4f5081134221ed693406682d95038f80efdeb4d
Salah Gueroui <salaheddinegueroui50@gmail.com>
2025-12-11 14:36:58 +0100

    Add SQL task solution

---
+++ Course-7-SQL-WHERE-Conditions/Search-Operator-task-01.sql
@@ -0,0 +1,9 @@
+/*
+Video: Course 7 – timestamp 35:57
+----------------------------------------
+-----SQL-TASK-----
+Find all customers whose first name starts with 'M'.
+*/
+
+------My-solution-----
+
