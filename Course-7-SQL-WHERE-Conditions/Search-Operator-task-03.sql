1c69cefe3d0f6b13af1988198e7a39cae0908528
Salah Gueroui <salaheddinegueroui50@gmail.com>
2025-12-11 14:46:19 +0100

    Add SQL task solution

--- Course-7-SQL-WHERE-Conditions/Search-Operator-task-03.sql
+++ Course-7-SQL-WHERE-Conditions/Search-Operator-task-03.sql
@@ -1,3 +1,12 @@
+/*
+Video: Course 7 – timestamp 37:25
+----------------------------------------
+-----SQL-TASK-----
+Find all customers whose first name contains 'r'.
+*/
+
+------My-solution-----
+
 select *
 from customers
 where first_name like '%r%'
