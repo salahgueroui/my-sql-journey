6f13e419050069fa85a72f96a254eee4c33138ee
Salah Gueroui <salaheddinegueroui50@gmail.com>
2025-12-12 22:09:23 +0100

    Add SQL task solution

--- Course-7-SQL-WHERE-Conditions/Search-Operator-task-04.sql
+++ Course-7-SQL-WHERE-Conditions/Search-Operator-task-04.sql
@@ -1,6 +1,6 @@
 /*
 Video: Course 7 – timestamp 38:22
-----------------------------------------
+---------------------------------------------
 -----SQL-TASK-----
 Find all customers whose first name has 'r' in the third position.
 */
