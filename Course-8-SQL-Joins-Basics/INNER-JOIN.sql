aecc50a0aae54d3f0b56fb82873bcf574f05bef9
Salah Gueroui <salaheddinegueroui50@gmail.com>
2025-12-17 16:56:32 +0100

    Add SQL solution

---
+++ Course-8-SQL-Joins-Basics/INNER-JOIN.sql
@@ -0,0 +1,14 @@
+/*
+Video: Course 8 - timestamp 9:43
+----------------------------------------
+-------------- SQL TASK --------------
+Retrieve all data from customers and orders as separate results
+----------------------------------------
+*/
+
+-------- My-Solution --------
+
+select *
+from customers;
+select *
+from orders;
