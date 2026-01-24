14eabf654dbd6e767b40463a89df04e6be01933a
Salah Gueroui <salaheddinegueroui50@gmail.com>
2026-01-25 00:17:14 +0100

    Add SQL solution

---
+++ Course-9-SQL-Joins-Advanced/CROSS-JOIN.sql
@@ -0,0 +1,15 @@
+/*
+Video: Course 9 – Advanced SQL Joins
+Timestamp: 17:37
+----------------------------------------
+-------------- SQL TASK --------------
+Generate all possible combinations of
+customers and orders
+----------------------------------------
+*/
+
+-------- My-Solution --------
+
+select *
+from customers
+cross join orders
