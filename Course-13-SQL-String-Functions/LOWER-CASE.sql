96932bb4789c02af56613eb2ec526e53f173e7c6
Salah Gueroui <salaheddinegueroui50@gmail.com>
2026-01-27 22:45:39 +0100

    Add SQL solution

---
+++ Course-13-SQL-String-Functions/LOWER-CASE.sql
@@ -0,0 +1,13 @@
+/*
+Video: Course 13 – SQL String Functions
+Timestamp: 04:43
+----------------------------------------
+-------------- SQL TASK --------------
+Convert the first name to lowercase
+----------------------------------------
+*/
+
+-------- My-Solution --------
+
+SELECT LOWER(first_name)as low_name
+from customers
