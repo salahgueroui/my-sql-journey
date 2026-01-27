7ae4867f37461c7088272b1fb22513713167fd5a
Salah Gueroui <salaheddinegueroui50@gmail.com>
2026-01-27 22:47:59 +0100

    Add SQL solution

---
+++ Course-13-SQL-String-Functions/UPPER-CASE.sql
@@ -0,0 +1,13 @@
+/*
+Video: Course 13 – SQL String Functions
+Timestamp: 05:15
+----------------------------------------
+-------------- SQL TASK --------------
+Convert the first name to uppercase
+----------------------------------------
+*/
+
+-------- My-Solution --------
+
+SELECT UPPER(first_name)as upp_name
+from customers
