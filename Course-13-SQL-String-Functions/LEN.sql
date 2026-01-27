c9691fc6020a4216c06c649beeeb8535c3beef62
Salah Gueroui <salaheddinegueroui50@gmail.com>
2026-01-27 23:29:48 +0100

    Add SQL solution

---
+++ Course-13-SQL-String-Functions/LEN.sql
@@ -0,0 +1,14 @@
+/*
+Video: Course 13 – SQL String Functions
+Timestamp: 16:03
+----------------------------------------
+-------------- SQL TASK --------------
+Calculate the length of each
+customer's first name
+----------------------------------------
+*/
+
+-------- My-Solution --------
+
+select len (first_name) as first_name_len
+from customers
