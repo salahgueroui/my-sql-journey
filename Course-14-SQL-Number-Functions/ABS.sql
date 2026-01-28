d6ff291aa8f5658a3ad39b16d738fd8344e45d7a
Salah Gueroui <salaheddinegueroui50@gmail.com>
2026-01-28 18:38:24 +0100

    Add SQL solution

---
+++ Course-14-SQL-Number-Functions/ABS.sql
@@ -0,0 +1,18 @@
+/*
+Video: Course 14 – SQL Number Functions
+Timestamp: 04:03
+----------------------------------------
+-------------- SQL TASK --------------
+Calculate the absolute value of numbers:
+- Absolute value of -10
+- Absolute value of 10
+----------------------------------------
+*/
+
+-------- My-Solution --
+
+select
+-10,
+10,
+abs(-10),
+abs(10)
