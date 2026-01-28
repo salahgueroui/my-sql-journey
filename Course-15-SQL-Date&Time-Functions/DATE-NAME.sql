cffae87cf8d54e0b2fce34414b1975fb8727204c
Salah Gueroui <salaheddinegueroui50@gmail.com>
2026-01-28 19:57:04 +0100

    Add SQL solution

---
+++ Course-15-SQL-Date&Time-Functions/DATE-NAME.sql
@@ -0,0 +1,22 @@
+/*
+Video: Course 15 – SQL Date & Time Functions
+----------------------------------------
+-------------- SQL TASK --------------
+Retrieve date names from CreationTime
+using DATENAME.
+
+For each order, display:
+- OrderID
+- CreationTime
+- Month name
+- Weekday name
+----------------------------------------
+*/
+
+-------- My-Solution --------
+
+select OrderID,
+	   datename(month,CreationTime) month,
+	   datename(weekday,CreationTime) weekday,
+	   datename(day,CreationTime) day
+from Sales.Orders
