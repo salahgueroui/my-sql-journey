f643b39e68f4bde79efa8079a966f21383886206
Salah Gueroui <salaheddinegueroui50@gmail.com>
2026-01-29 21:57:55 +0100

    Add SQL solution

---
+++ Course-16-SQL-Date-and-Time-Functions/CAST.sql
@@ -0,0 +1,15 @@
+/*
+Video: Course 16 – SQL Date & Time Functions CAST
+Timestamp: —
+----------------------------------------
+-------------- SQL TASK --------------
+Convert CreationTime to DATE using CAST
+(remove the time part)
+----------------------------------------
+*/
+
+-------- My-Solution --------
+
+select CreationTime,
+cast(creationtime as date)
+from sales.Orders
