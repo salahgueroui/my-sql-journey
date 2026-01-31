fb55b5a5e04de74aee850be72fbe4c1fecc0357a
Salah Gueroui <salaheddinegueroui50@gmail.com>
2026-01-31 16:58:16 +0100

    Add SQL solution

--- Course-17-SQL-Date-and-Time-Functions/DATEDIFF.sql
+++ Course-17-SQL-Date-and-Time-Functions/DATEDIFF.sql
@@ -11,3 +11,21 @@ Calculate the age of employees.
 
 select EmployeeID,DATEDIFF(year,BirthDate,getdate()) age_employee 
 from Sales.Employees
+
+/*
+Video: Course 17 – SQL Date & Time Functions
+Timestamp: 08:57
+----------------------------------------
+-------------- SQL TASK --------------
+Find the average shipping duration in days for each month.
+----------------------------------------
+*/
+
+-------- My-Solution --------
+
+SELECT 
+    FORMAT(OrderDate, 'MMM') AS [month],
+    AVG(DATEDIFF(day, OrderDate, ShipDate)) AS avg_shipping
+FROM Sales.Orders
+GROUP BY FORMAT(OrderDate, 'MMM'), MONTH(OrderDate)
+
