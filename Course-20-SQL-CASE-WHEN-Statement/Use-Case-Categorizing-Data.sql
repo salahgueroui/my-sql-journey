feb36913453eb33f45c282210cb0b6cec485ef73
Salah Gueroui <salaheddinegueroui50@gmail.com>
2026-02-01 16:53:39 +0100

    Add SQL solution

---
+++ Course-20-SQL-CASE-WHEN-Statement/Use-Case-Categorizing-Data.sql
@@ -0,0 +1,29 @@
+/*
+Video: Course 20 – SQL CASE WHEN Statement
+Topic: Categorizing Data
+Timestamp: 11:34
+----------------------------------------
+-------------- SQL TASK --------------
+Generate a report showing the total sales for each category:
+
+- High: If the sales are higher than 50
+- Medium: If the sales are between 20 and 50
+- Low: If the sales are equal to or lower than 20
+
+Sort the result from lowest to highest.
+----------------------------------------
+*/
+
+-------- My-Solution --------
+
+
+select category,sum(sales) as total_sales
+from (select OrderID,Sales,
+		case 
+		when Sales > 50 then 'high'  
+		when sales between 21 and 50 then 'medium'
+    	else 'low'
+	end category
+	from Sales.Orders)t
+group by category
+order by sum(sales)
