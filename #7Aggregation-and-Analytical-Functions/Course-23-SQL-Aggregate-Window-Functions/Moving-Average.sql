8ba9cf8e1baf32755592ddd872077ad4c9ac15af
Salah Gueroui <salaheddinegueroui50@gmail.com>
2026-02-19 15:36:48 +0100

    Add SQL solution

---
+++ #7Aggregation-and-Analytical-Functions/Course-23-SQL-Aggregate-Window-Functions/Moving-Average.sql
@@ -0,0 +1,13 @@
+/*
+Video: Course 23 – SQL Aggregate Window Functions
+Topic: Moving Average (Window Frame)
+Timestamp: 55:25
+----------------------------------------
+-------------- SQL TASK --------------
+Calculate the moving average of sales
+for each product over time.
+----------------------------------------
+*/
+
+-------- My-Solution --------
+
