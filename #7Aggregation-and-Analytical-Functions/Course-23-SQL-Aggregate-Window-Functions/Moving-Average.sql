f044dc4039ce5013c1239c66742cb36b81872439
Salah Gueroui <salaheddinegueroui50@gmail.com>
2026-02-19 16:48:39 +0100

    Add SQL solution

--- #7Aggregation-and-Analytical-Functions/Course-23-SQL-Aggregate-Window-Functions/Moving-Average.sql
+++ #7Aggregation-and-Analytical-Functions/Course-23-SQL-Aggregate-Window-Functions/Moving-Average.sql
@@ -17,3 +17,27 @@ select OrderID,
 	   avg(Sales) over (partition by productid ) AvgSalesByProducts,
 	   avg(Sales) over (partition by productid order by orderdate ) MovingAvg
 FROM Sales.Orders
+
+/*
+Video: Course 23 – SQL Aggregate Window Functions
+Topic: Moving Average with Window Frame
+Timestamp: 59:36
+----------------------------------------
+-------------- SQL TASK --------------
+Calculate the moving average of sales 
+for each product over time, including 
+only the next order.
+----------------------------------------
+*/
+
+-------- My-Solution --------
+
+select OrderID,
+	   OrderDate,
+	   ProductID,
+	   Sales,
+	   avg(Sales) over (partition by productid ) AvgSalesByProducts,
+	    avg(Sales) over (partition by productid order by orderdate) MovingAvg,
+	   avg(Sales) over (partition by productid order by orderdate 
+	   rows between current row and 1following) RollingAvg
+FROM Sales.Orders
