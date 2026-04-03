969768be384e4df2125f1da0eff288146c089c01
Salah Gueroui <salaheddinegueroui50@gmail.com>
2026-04-03 18:43:21 +0200

    Add SQL Solution

--- #8Advanced-SQL-Techniques/Course-27-Subquery/4-WHERE Clause Subquery/2-logical operator subquery/1-IN Operator Subquery.sql
+++ #8Advanced-SQL-Techniques/Course-27-Subquery/4-WHERE Clause Subquery/2-logical operator subquery/1-IN Operator Subquery.sql
@@ -15,3 +15,22 @@ from sales.Orders
 where CustomerID in (select CustomerID 
                     from Sales.Customers 
                     where Country ='Germany')
+
+
+/*
+Video: Course 27 – SQL Subquery (Visually Explained)
+Topic: Subqueries
+Timestamp: 47:31
+----------------------------------------
+-------------- SQL TASK --------------
+Show the details of orders for customers who are not from Germany.
+----------------------------------------
+*/
+
+-------- My-Solution --------
+
+select *
+from sales.Orders
+where CustomerID not in (select CustomerID 
+                        from Sales.Customers 
+                        where Country ='Germany')
