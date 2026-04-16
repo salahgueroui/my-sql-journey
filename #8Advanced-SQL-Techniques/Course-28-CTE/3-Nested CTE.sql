533137678dd1a0bfb1e43a601453a30a7d338afa
Salah Gueroui <salaheddinegueroui50@gmail.com>
2026-04-16 15:48:56 +0100

    Add SQL Solution

--- #8Advanced-SQL-Techniques/Course-28-CTE/3-Nested CTE.sql
+++ #8Advanced-SQL-Techniques/Course-28-CTE/3-Nested CTE.sql
@@ -21,5 +21,22 @@ from seles_per_customer )
  
 select * from rank_customer
 
+-- segment customers based on their total sales
+
+with seles_per_customer as
+(
+select CustomerID,sum(Sales) as total_sales_per_customer 
+from Sales.Orders
+group by CustomerID
+)
+,segment_customer as 
+(select *, case when total_sales_per_customer > 100 then 'high' 
+            when total_sales_per_customer > 50 then 'medium' 
+            else 'low' end as segment 
+from seles_per_customer )
+
+select * from segment_customer
+
 
  
+
