61f2c79c119e19e43734e105753445b80dacb8c6
Salah Gueroui <salaheddinegueroui50@gmail.com>
2026-04-17 17:50:27 +0100

    Add SQL Solution

---
+++ #8Advanced-SQL-Techniques/Course-29-Views/1-task.sql
@@ -0,0 +1,25 @@
+/*
+Video: SQL Views (Visually Explained)
+Timestamp: 20:24
+----------------------------------------
+-------------- SQL TASK --------------
+Find the running total of sales for each month
+----------------------------------------
+*/
+
+-------- My-Solution --
+
+WITH spm AS (
+    SELECT 
+        MONTH(ShipDate) AS month,
+        SUM(Sales) AS sales
+    FROM sales.Orders
+    GROUP BY MONTH(ShipDate)
+),
+mom as 
+(SELECT 
+    *,
+    SUM(sales) OVER (ORDER BY month) AS running_total
+FROM spm)
+
+select * from mom
