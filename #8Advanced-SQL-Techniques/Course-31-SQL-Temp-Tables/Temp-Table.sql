85653ad4877104413ef7589328e157f8da22447d
Salah Gueroui <salaheddinegueroui50@gmail.com>
2026-04-19 18:57:37 +0100

    Add SQL Solution

---
+++ #8Advanced-SQL-Techniques/Course-31-SQL-Temp-Tables/Temp-Table.sql
@@ -0,0 +1,14 @@
+--step 1: load data into temp table
+select *
+into #tempOrders
+from sales.orders
+
+--step 2: transform data in temp table
+delete from #tempOrders
+where orderstatus = 'delivered'
+
+--step 3: load data into permanent table
+select *
+into sales.deliveredOrders
+from #tempOrders
+
