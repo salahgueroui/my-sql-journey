cf3b330d6896770783c7fb2909ffafb619225ee2
Salah Gueroui <salaheddinegueroui50@gmail.com>
2026-05-18 18:10:47 +0100

    Add SQL Solution

--- #8Advanced-SQL-Techniques/6-SQL Indexes/1-Course-35-Clustered vs Nonclustered/1-Create index.sql
+++ #8Advanced-SQL-Techniques/6-SQL Indexes/1-Course-35-Clustered vs Nonclustered/1-Create index.sql
@@ -1,3 +1,21 @@
 --create index on sales.DBCustomers table on CustomerID column
 create clustered index idx_DBCustomers_CustomerID 
 on sales.DBCustomers (CustomerID)
+
+
+select * from sales.DBCustomers
+where lastname = 'brown'
+--create nonclustered index on sales.DBCustomers table on LastName column
+create nonclustered index idx_DBCustomers_LastName 
+on sales.DBCustomers (LastName)
+
+
+select * from sales.DBCustomers
+where lastname = 'anna'
+--create nonclustered index on sales.DBCustomers table on FirstName column
+create index idx_DBCustomers_FirstName 
+on sales.DBCustomers (FirstName)
+
+
+
+
--- #8Advanced-SQL-Techniques/6-SQL Indexes/1-Course-35-Clustered vs Nonclustered/2-Drop index.sql
+++ #8Advanced-SQL-Techniques/6-SQL Indexes/1-Course-35-Clustered vs Nonclustered/2-Drop index.sql
@@ -1,3 +1,4 @@
 --drop index on sales.DBCustomers table on CustomerID column
 drop index idx_DBCustomers_CustomerID 
 on sales.DBCustomers 
+
